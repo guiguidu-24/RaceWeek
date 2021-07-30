2c75e67f7079ab9279850624e8971bf2993040dc
Guillaume Houchou <guillaume.houchou@gmail.com>
2021-07-30 21:21:05 +0200

    Add an history system

--- main.py
+++ main.py
@@ -15,14 +15,23 @@ delta_time = schedule[0] - now
 
 if delta_time.days <= 7:
     logger.debug('Found a race in the week')
-    tweet = 'C\'est une semaine de Grand Prix !\n' + names[0] + "\n" + locations[0] + "\n"
+    last_tweet_name = ""
+    with open("tweet_history.txt", 'r', encoding='utf-8') as history:
+        last_tweet_name = history.read().split('\n')[-2]
 
-    for i in range(len(schedule)):
-        if names[i] == names[0]:
-            tweet += f"{sessions[i]} : {schedule[i].strftime('%H:%M')}\n"
-            continue
-        logger.info('Send the tweet')
-        Twitter().post(tweet)
-        break
+    if last_tweet_name != names[0]:
+        logger.debug('Found a race to share')
+        tweet = 'C\'est une semaine de Grand Prix !\n' + names[0] + "\n" + locations[0] + "\n"
+        for i in range(len(schedule)):
+            if names[i] == names[0]:
+                tweet += f"{sessions[i]} : {schedule[i].strftime('%H:%M')}\n"
+                continue
+            logger.info('Send the tweet')
+            Twitter().post(tweet)
+            with open('tweet_history.txt', 'a', encoding='utf-8') as tweet_history:
+                tweet_history.write(f"{names[0]}\n")
+            break
+    else:
+        logger.warning('The race have already been posted')
 
 logger.info('Close the app\n')
